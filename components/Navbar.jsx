92548712d5ca409836d03ac8468d7939b9b99a85
Nicole Struggia <103970504+Nicolettastr@users.noreply.github.com>
2023-07-11 12:35:55 +0200

    fixing some styles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -26,6 +26,7 @@ const NavbarComponent = () => {
                 <Nav.Link href="about">About Me</Nav.Link>
                 <Nav.Link href="projects">Projects</Nav.Link>
                 <Nav.Link href="resume">Resume</Nav.Link>
+                <Nav.Link href="#contactSection">Contact Me</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
